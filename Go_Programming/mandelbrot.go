38500a83fcbddade03461bf17d7eddc657f9dddd
Stanislaw Alam <stan.alam@gmail.com>
2017-01-18 14:34:05 -0500

    updated mandelbrot example

---
+++ Go_Programming/mandelbrot.go
@@ -0,0 +1,31 @@
+package main
+
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+)
+
+
+func main() {
+
+	const (
+		xmin, ymin, ymax = -2, -2, +2, +2
+		width, height    = 1024, 1024
+		)
+	img := image.NewRGBA( image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(px)/width*(xmax-)
+	}
+
+
+
+
+
+
+
+
+
+}
